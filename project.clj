5a90452d3818814d3e33c23eb01d1cc18fe70a51
Alexander Scott <xander@axrs.io>
2021-03-09 20:37:42 +1000

    0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tech.ardour/utensil "0.0.1-SNAPSHOT"
+(defproject tech.ardour/utensil "0.0.1"
   :description "Ardour Tech Utilities and Helpers"
   :url "https://github.com/ArdourTech/utensil"
   :license {:name         "Eclipse Public License - v 1.0"
